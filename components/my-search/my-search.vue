18eb98160dcc61384b8852c440a72e420e6517de
PooYooGitHub <ysy3083446265@gmail.com>
2024-07-05 10:53:20 +0800

    基本完成搜索样式

--- components/my-search/my-search.vue
+++ components/my-search/my-search.vue
@@ -45,26 +45,26 @@ export default {
 </script>
 
 <style lang="scss">
-// .my-search-container {
-//   background-color: #c00000;
-//   height: 50px;
-//   padding: 0 10px;
-//   display: flex;
-//   align-items: center;
-// }
+.my-search-container {
+  // background-color: #c00000;
+  height: 50px;
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
+}
 
-// .my-search-box {
-//   height: 36px;
-//   background-color: #ffffff;
-//   border-radius: 15px;
-//   width: 100%;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
+.my-search-box {
+  height: 36px;
+  background-color: #ffffff;
+  // border-radius: 15px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-//   .placeholder {
-//     font-size: 15px;
-//     margin-left: 5px;
-//   }
-// }
+  .placeholder {
+    font-size: 15px;
+    margin-left: 5px;
+  }
+}
 </style>
